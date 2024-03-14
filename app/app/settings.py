191b628e7226acc28c7a6c76c86c4994ace61cd4
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2024-03-14 15:32:47 +0300

    fix settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = get_random_secret_key()
 
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG", 1)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
 
